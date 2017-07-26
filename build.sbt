b4bffc126f6eb7e6256316531443f44ef319af14
Chen Mawson <chen@Chens-MacBook-Pro.local>
2017-07-26 16:02:54 +1000

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "msgapp"
+
+organization := "com.github.chenmawson"
+
+version := "0.0.1"
+
+scalaVersion := "2.11.11"
+
+libraryDependencies ++= List(
+    "org.seleniumhq.selenium" % "selenium-server" % "3.4.0"
+)
---
+++ src/test/java/msgapp/LoginTest.java
@@ -0,0 +1,43 @@
+package msgapp;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+/**
+ * Created by chen on 19/7/17.
+ */
+public class LoginTest {
+    @Test
+    public void testLogIn() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.smsfun.com.au/joobz.php");
+        WebElement username = driver.findElement(By.name("mobile"));
+        System.out.println(System.getenv("MSGAPP_USERNAME"));
+        username.sendKeys(System.getenv("MSGAPP_USERNAME"));
+        WebElement pwd = driver.findElement(By.name("pwd"));
+        pwd.sendKeys(System.getenv("MSGAPP_PWD"));
+        driver.findElement(By.name("submit")).click();
+        WebElement joobzNum = driver.findElement(By.className("notice-joobz"));
+        System.out.println(joobzNum.getText());
+        //driver.findElement(By.linkText("Joobz")).click();
+        //driver.findElement(By.linkText("Earn Joobz")).click();
+        Actions builder= new Actions(driver);
+        builder.moveToElement(driver.findElement(By.linkText("Joobz"))).perform();
+        driver.findElement(By.linkText("Earn Joobz")).click();
+        driver.findElement(By.linkText("Daily Joobz")).click();
+        String taskContainerText = driver.findElement(By.className("joobz-task-container")).getText();
+        if (taskContainerText.contains("You already have claimed")){
+            System.out.println(taskContainerText);
+        } else {
+            driver.findElement(By.cssSelector("input[value='Claim Your Daily Joobz']")).click();
+            System.out.println("Joobz claimed");
+        }
+
+    }
+
+
+}
