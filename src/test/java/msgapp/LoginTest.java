07865160bf8e84d521d361810ca3fe2b856dda49
Chen Mawson <chen@loftinspace.com.au>
2017-08-17 15:27:32 +1000

    added wait for element

--- src/test/java/msgapp/LoginTest.java
+++ src/test/java/msgapp/LoginTest.java
@@ -2,25 +2,38 @@ package msgapp;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 /**
  * Created by chen on 19/7/17.
  */
 public class LoginTest {
-    @Test
-    public void testLogIn() {
 
-        WebDriver driver;
+    private WebDriver driver;
+
+    public LoginTest() {
         if ("true".equals(System.getenv("TRAVIS"))){
             driver = new FirefoxDriver();
         } else {
             driver = new ChromeDriver();
         }
+    }
+    private WebElement waitForElement(By by) {
+        Wait<WebDriver> wait = new FluentWait<>(driver).withTimeout(30, SECONDS).pollingEvery(5, SECONDS).ignoring(NoSuchElementException.class);
+        return wait.until(driver -> driver.findElement(by));
+    }
+
+    @Test
+    public void testLogIn() {
         driver.get("http://www.smsfun.com.au/joobz.php");
         WebElement username = driver.findElement(By.name("mobile"));
         username.sendKeys(System.getenv("MSGAPP_USERNAME"));
@@ -33,8 +46,9 @@ public class LoginTest {
         //driver.findElement(By.linkText("Earn Joobz")).click();
         Actions builder= new Actions(driver);
         builder.moveToElement(driver.findElement(By.linkText("Joobz"))).perform();
-        driver.findElement(By.linkText("Earn Joobz")).click();
-        driver.findElement(By.linkText("Daily Joobz")).click();
+        waitForElement(By.linkText("Earn Joobz")).click();
+        //driver.findElement(By.linkText("Earn Joobz")).click();
+        waitForElement(By.linkText("Daily Joobz")).click();
         String taskContainerText = driver.findElement(By.className("joobz-task-container")).getText();
         if (taskContainerText.contains("You already have claimed")){
             System.out.println(taskContainerText);
@@ -44,6 +58,8 @@ public class LoginTest {
         }
 
     }
+
+
 
 
 }
